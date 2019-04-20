8743f764ab3c0740f7edfcc4f1a8562f54d08c17
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2019-04-20 21:51:56 +0200

    some fixes

--- setup.py
+++ setup.py
@@ -12,6 +12,7 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/sebrut/pygrocy",
+    license='MIT',
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
