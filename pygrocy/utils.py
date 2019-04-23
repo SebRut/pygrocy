0e42ec7789d8285197afa13317ac4eee68706545
SebRut <sebastian.rutofski@gmail.com>
2019-04-23 08:39:13 +0200

    code quality improvements
    Signed-off-by: SebRut <sebastian.rutofski@gmail.com>

--- pygrocy/utils.py
+++ pygrocy/utils.py
@@ -2,27 +2,24 @@ import iso8601
 
 
 def parse_date(input):
-    if input == None:
+    if input is None:
         return None
-    else:
-        return iso8601.parse_date(input)
+    return iso8601.parse_date(input)
 
 
 def parse_int(input, default_value=None):
-    if input == None:
+    if input is None:
         return default_value
-    else:
-        try:
-            return int(input)
-        except ValueError:
-            return default_value
+    try:
+        return int(input)
+    except ValueError:
+        return default_value
 
 
 def parse_float(input, default_value=None):
-    if input == None:
+    if input is None:
         return default_value
-    else:
-        try:
-            return float(input)
-        except ValueError:
-            return default_value
+    try:
+        return float(input)
+    except ValueError:
+        return default_value
