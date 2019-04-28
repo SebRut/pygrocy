e8d195e1fc69ff841c0366622aebae1f4e6b7052
SebRut <sebastian.rutofski@gmail.com>
2019-04-28 11:11:52 +0200

    version bump

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygrocy",
-    version="0.3.0",
+    version="0.3.2",
     author="Sebastian Rutofski",
     author_email="kontakt@sebastian-rutofski.de",
     description="",
