094f614a4efc86e6fa0ee8b9dfff676a76e3dee5
SebRut <sebastian.rutofski@gmail.com>
2019-04-28 11:10:34 +0200

    move dependencies to setup.py

--- setup.py
+++ setup.py
@@ -14,6 +14,10 @@ setuptools.setup(
     url="https://github.com/sebrut/pygrocy",
     license='MIT',
     packages=setuptools.find_packages(),
+    install_requires=[
+        "requests",
+        "iso8601"
+    ],
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
