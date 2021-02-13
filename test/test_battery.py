c31abba7a44b818892874024216086dbfe107d45
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2021-02-13 22:57:04 +0100

    extend coverage

--- test/test_battery.py
+++ test/test_battery.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 import pytest
 
 
@@ -7,6 +9,7 @@ class TestBattery:
         batteries = grocy.batteries()
 
         assert len(batteries) == 4
+        assert isinstance(batteries[0].last_tracked_time, datetime)
 
     @pytest.mark.vcr
     def test_get_battery_details_valid(self, grocy):
@@ -17,9 +20,9 @@ class TestBattery:
         assert battery.description == "Warranty ends 2023"
         assert battery.used_in == "TV remote control"
         assert battery.charge_interval_days == 0
-        assert battery.created_timestamp.year == 2021
-        assert battery.created_timestamp.month == 2
-        assert battery.created_timestamp.day == 13
+        assert isinstance(battery.created_timestamp, datetime)
+        assert isinstance(battery.last_charged, datetime)
+        assert battery.next_estimated_charge_time is None
         assert battery.userfields is None
         assert battery.charge_cycles_count == 4
 
