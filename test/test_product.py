aaa23f7d6dbac79464b7656ffb902589f5d42b67
Sebastian Rutofski <SebRut@users.noreply.github.com>
2021-02-15 16:34:48 +0100

    fix error if product has no barcodes (#149)

--- test/test_product.py
+++ test/test_product.py
@@ -3,13 +3,23 @@ import pytest
 
 class TestProduct:
     @pytest.mark.vcr
-    def test_product_get_details_valid(self, grocy, grocy_api_client):
+    def test_product_get_details_valid(self, grocy):
         product = grocy.product(10)
 
         assert product.name == "Cheese"
         assert product.available_amount == 5
         assert len(product.barcodes) == 0
         assert product.product_group_id == 6
+
+    @pytest.mark.vcr
+    def test_product_no_barcodes(self, grocy):
+        stock = grocy.stock()
+        product = next(prod for prod in stock if prod.id == 2)
+
+        assert product.name == "Chocolate"
+        assert product.product_barcodes is not None
+        assert isinstance(product.product_barcodes, list)
+        assert len(product.product_barcodes) == 0
 
     @pytest.mark.vcr
     def test_product_get_details_non_existant(self, grocy):
