e4a3bbf93fc782de2e6b5ae10bc0459991bcac65
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2021-08-23 11:10:13 +0200

    add error case test

--- test/test_tasks.py
+++ test/test_tasks.py
@@ -2,6 +2,8 @@ from datetime import datetime
 
 import pytest
 
+from pygrocy.errors import GrocyError
+
 
 class TestTasks:
     @pytest.mark.vcr
@@ -19,3 +21,11 @@ class TestTasks:
     @pytest.mark.vcr
     def test_complete_task_valid(self, grocy):
         grocy.complete_task(4, done_time=datetime.now())
+
+    @pytest.mark.vcr
+    def test_complete_task_invalid(self, grocy):
+        with pytest.raises(GrocyError) as exc_info:
+            grocy.complete_task(1000)
+
+        error = exc_info.value
+        assert error.status_code == 400
