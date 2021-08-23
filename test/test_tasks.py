7814cba73a0d1821bcd042ba5c99e381802d13f9
Sebastian Rutofski <SebRut@users.noreply.github.com>
2021-08-23 11:13:07 +0200

    make done_time optional for complete_task (#181)

--- test/test_tasks.py
+++ test/test_tasks.py
@@ -1,4 +1,8 @@
+from datetime import datetime
+
 import pytest
+
+from pygrocy.errors import GrocyError
 
 
 class TestTasks:
@@ -9,3 +13,19 @@ class TestTasks:
         assert len(tasks) == 6
         assert tasks[0].id == 1
         assert tasks[0].name == "Repair the garage door"
+
+    @pytest.mark.vcr
+    def test_complete_task_valid_with_defaults(self, grocy):
+        grocy.complete_task(3)
+
+    @pytest.mark.vcr
+    def test_complete_task_valid(self, grocy):
+        grocy.complete_task(4, done_time=datetime.now())
+
+    @pytest.mark.vcr
+    def test_complete_task_invalid(self, grocy):
+        with pytest.raises(GrocyError) as exc_info:
+            grocy.complete_task(1000)
+
+        error = exc_info.value
+        assert error.status_code == 400
