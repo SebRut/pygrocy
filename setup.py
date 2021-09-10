9671b59aadc518c0f0539032458b88010d9853c9
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2021-09-10 19:12:12 +0200

    v1.0.0 prep

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="pygrocy",
-    version="0.30.0",
+    version="1.0.0",
     author="Sebastian Rutofski",
     author_email="kontakt@sebastian-rutofski.de",
     description="",
