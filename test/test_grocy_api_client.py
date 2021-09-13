b14f5ba303e8410de10ed6a9e1621dfe66270c58
Sebastian Rutofski <sebastian.rutofski@gmail.com>
2021-09-13 11:01:37 +0200

    add tests for path

--- test/test_grocy_api_client.py
+++ test/test_grocy_api_client.py
@@ -9,3 +9,15 @@ class TestGrocyApiClient:
     def test_url_and_port(self):
         client = GrocyApiClient(api_key="", base_url="http://grocy.de", port=1234)
         assert client._base_url == "http://grocy.de:1234/api/"
+
+    def test_url_and_port_and_path(self):
+        client = GrocyApiClient(
+            api_key="", base_url="http://grocy.de", port=1234, path="my/custom/path"
+        )
+        assert client._base_url == "http://grocy.de:1234/my/custom/path/api/"
+
+    def test_url_and_path(self):
+        client = GrocyApiClient(
+            api_key="", base_url="http://grocy.de", path="my/custom/path"
+        )
+        assert client._base_url == "http://grocy.de:9192/my/custom/path/api/"
